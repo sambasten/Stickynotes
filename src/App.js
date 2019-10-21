95650dcba8f0f771210716d9e4067dedc50d8573
sambasten <akinwumisamuel@gmail.com>
2019-10-21 12:58:07 +0100

    add board parent class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Notes extends Component {
+
+    constructor(props){
+    super(props);
+    this.state= {editing:false ,
+                 inputValue: ''};
+    }
+
+    save = () =>{
+      var val= this.refs.newText.value;
+      alert ("Save Node value: " + val);
+      this.setState({editing:false});
+    }
+    edit = () =>{
+       this.setState({editing:true});
+      }
+
+    remove = () =>{
+        alert("Removing note");
+      }
+
+    // updateInputValue = (evt) => {
+    //     this.setState({inputValue: evt.target.value
+    //     });
+    //   }
+
+    handleForm = () => {
+       return(
+            <div className="note">
+              <textarea className="form-control" defaultValue= {this.props.children}  ref= "newText"></textarea>
+              <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk"/>
+              </div>
+          );
+      }
+
+    handleDisplay = () => {
+        return(
+          <div className="note">
+              <p>{this.props.children}</p>
+              <span>
+                  <button onClick={this.edit} className ="btn btn-primary glyphicon glyphicon-pencil"/>
+                  <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
+              </span>  
+              </div>
+              );
+
 }
 
-export default App;
+    render(){
+         if (this.state.editing){
+             return this.handleForm();
+         }
+         else {
+          return this.handleDisplay();
+         }
+    }
+
+  }
+    
+class Board extends Component{
+    constructor(props){
+    super(props);
+    this.state={
+      notes: ['Call Bill',
+              'Email Lisa',
+              'Make Dental Appointment'
+              ]
+    };
+  }
+
+    proptypes = {
+      count (props, propsName){
+          if (typeof props[propsName] != "number"){
+            return new Error ('The count property must be a number')
+          }
+          if (props[propsName] > 100){
+            return new Error ('Creating ' + props[propsName] + " notes is ridiculous")
+          }
+      }
+    }
+
+    render(){
+        return (<div className="board">
+                   {this.state.notes.map(item => (
+                    <Notes key={item}>{item}</Notes>
+                    ))}
+                </div>);
+    }
+}
+
+export {Board, Notes};
+
